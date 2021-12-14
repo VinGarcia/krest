b995e34c9e920aec08ada83af3125e89f4a5e00c
Vinícius Garcia <vingarcia00@gmail.com>
2021-12-14 19:40:10 -0300

    Add some comments required by golint on mocks.go

--- mocks.go
+++ mocks.go
@@ -2,6 +2,7 @@ package krest
 
 import "context"
 
+// Mock mocks the krest.Provider interface with a configurable structure
 type Mock struct {
 	GetFn    func(ctx context.Context, url string, data RequestData) (resp Response, err error)
 	PostFn   func(ctx context.Context, url string, data RequestData) (resp Response, err error)
@@ -10,6 +11,7 @@ type Mock struct {
 	DeleteFn func(ctx context.Context, url string, data RequestData) (resp Response, err error)
 }
 
+// Get mocks the krest.Provider.Get method
 func (m Mock) Get(ctx context.Context, url string, data RequestData) (resp Response, err error) {
 	if m.GetFn != nil {
 		return m.GetFn(ctx, url, data)
@@ -17,6 +19,7 @@ func (m Mock) Get(ctx context.Context, url string, data RequestData) (resp Respo
 	return Response{}, nil
 }
 
+// Post mocks the krest.Provider.Post method
 func (m Mock) Post(ctx context.Context, url string, data RequestData) (resp Response, err error) {
 	if m.PostFn != nil {
 		return m.PostFn(ctx, url, data)
@@ -24,6 +27,7 @@ func (m Mock) Post(ctx context.Context, url string, data RequestData) (resp Resp
 	return Response{}, nil
 }
 
+// Put mocks the krest.Provider.Put method
 func (m Mock) Put(ctx context.Context, url string, data RequestData) (resp Response, err error) {
 	if m.PutFn != nil {
 		return m.PutFn(ctx, url, data)
@@ -31,6 +35,7 @@ func (m Mock) Put(ctx context.Context, url string, data RequestData) (resp Respo
 	return Response{}, nil
 }
 
+// Patch mocks the krest.Provider.Patch method
 func (m Mock) Patch(ctx context.Context, url string, data RequestData) (resp Response, err error) {
 	if m.PatchFn != nil {
 		return m.PatchFn(ctx, url, data)
@@ -38,6 +43,7 @@ func (m Mock) Patch(ctx context.Context, url string, data RequestData) (resp Res
 	return Response{}, nil
 }
 
+// Delete mocks the krest.Provider.Delete method
 func (m Mock) Delete(ctx context.Context, url string, data RequestData) (resp Response, err error) {
 	if m.DeleteFn != nil {
 		return m.DeleteFn(ctx, url, data)
